2e74533922d629b299bd762d8321bd8734cf64bc
ken.kamiya <ken.kamiya@kraken.tech>
2025-04-15 20:53:13 +0900

    Change config to kraken-mac

--- docs/setup.sh
+++ docs/setup.sh
@@ -18,30 +18,30 @@ python manage.py runserver 127.0.0.1:8890
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"email":"asd@asd.com","password":"xyz","username":"asd3"}' \
-  http://192.168.10.2:8890/api/v2/user/signup/
+  http://192.168.10.13:8890/api/v2/user/signup/
 
 # Run API without JWT
 curl -H "Content-Type: application/json" \
   --request POST \
   --data '{"st_chat_group_name":"Chat Group1", "bl_personal":"True"}' \
-  http://192.168.10.2:8890/api/v2/chatGroup/create/
+  http://192.168.10.13:8890/api/v2/chatGroup/create/
 
 # Run API with wrong JWT
 curl -H "Content-Type: application/json" \
   -H "Authorization: Bearer ABC" \
   --request POST \
   --data '{"st_chat_group_name":"Chat Group1", "bl_personal":"True"}' \
-  http://192.168.10.2:8890/api/v2/chatGroup/create/
+  http://192.168.10.13:8890/api/v2/chatGroup/create/
 
 # Login and get JWT
-ACCESS=$(curl --header "Content-Type: application/json" --request POST --data '{"username":"asd3","password":"xyz"}' http://192.168.10.2:8890/api/v2/user/login/ | jq -r '.access')
+ACCESS=$(curl --header "Content-Type: application/json" --request POST --data '{"username":"asd3","password":"xyz"}' http://192.168.10.13:8890/api/v2/user/login/ | jq -r '.access')
 
 # Create new chat groups
 curl -H "Content-Type: application/json" \
   -H "Authorization: Bearer ${ACCESS}" \
   --request POST \
   --data '{"st_chat_group_name":"Chat Group1", "bl_personal":"True"}' \
-  http://192.168.10.2:8890/api/v2/chatGroup/create/
+  http://192.168.10.13:8890/api/v2/chatGroup/create/
 
 # Clear DB Users
 python manage.py shell
@@ -51,22 +51,22 @@ python manage.py shell
 # Run API after deleting the user
 curl -H "Authorization: Bearer ${ACCESS}" \
   --request GET \
-  http://192.168.10.2:8890/api/v2/test/
+  http://192.168.10.13:8890/api/v2/test/
 
 # Get all users
 curl -H "Authorization: Bearer ${ACCESS}" \
   --request GET \
-  http://192.168.10.2:8890/api/v2/user/listAllUsers/
+  http://192.168.10.13:8890/api/v2/user/listAllUsers/
 
 # Get all chat groups
 curl -H "Authorization: Bearer ${ACCESS}" \
   --request GET \
-  http://192.168.10.2:8890/api/v2/chatGroup/myGroups/
+  http://192.168.10.13:8890/api/v2/chatGroup/myGroups/
 
 # Join chat group
 curl -H "Content-Type: application/json" \
   -H "Authorization: Bearer ${ACCESS}" \
   --request POST \
   --data '{"st_chat_group_name":"Chat Group1"}' \
-  http://192.168.10.2:8890/api/v2/chatGroup/join/
+  http://192.168.10.13:8890/api/v2/chatGroup/join/
 
