6d13166ce54a8238052df460fa25a989cb56032d
ken.kamiya <ken.kamiya@kraken.tech>
2025-09-22 00:19:19 +0900

    Move note variables from note-main to app root

--- origin/urls/note/urls.py
+++ origin/urls/note/urls.py
@@ -6,7 +6,7 @@ from origin.views.note.note_views import *
 urlpatterns = [
     path("api/v2/note/personal/", PersonalNoteMasterView.as_view(), name="personal_note"),
     path(
-        "api/v2/note/personal/single",
+        "api/v2/note/personal/single/",
         SinglePersonalNoteView.as_view(),
         name="personal_single_note",
     ),
