5557c3111e294c8eeba9d526ac13a26d097474a3
ken.kamiya <ken.kamiya@kraken.tech>
2026-01-16 21:46:24 +0900

    Add Note Favorites Function

--- origin/serializers/note/note_serializers.py
+++ origin/serializers/note/note_serializers.py
@@ -3,6 +3,7 @@ from origin.models.note.common_note_models import *
 from origin.models.note.personal_note_models import *
 from origin.models.note.task_note_models import *
 from origin.models.note.chat_note_models import *
+from origin.models.note.favorite_note_models import *
 
 
 class PersonalNoteMasterSerializer(serializers.ModelSerializer):
@@ -44,4 +45,10 @@ class ChatNoteAttachmentFactSerializer(serializers.ModelSerializer):
 class NotePermissionMasterSerializer(serializers.ModelSerializer):
     class Meta:
         model = NotePermissionMaster
+        fields = "__all__"
+
+
+class NoteFavoriteMasterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoteFavoriteMaster
         fields = "__all__"
