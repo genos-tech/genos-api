6f7b7e8b662684abd393ff64e4b9a92fef1d3f15
ken.kamiya <ken.kamiya@kraken.tech>
2026-02-01 17:16:14 +0900

    Initial commit

--- origin/models/__init__.py
+++ origin/models/__init__.py
@@ -3,5 +3,6 @@ from .chat.reaction_models import *
 from .chat.mention_models import *
 from .chat.dm_models import *
 from .chat.gm_models import *
+from .chat.mdm_models import *
 from .chat.pm_models import *
 from .chat.todo_models import *
